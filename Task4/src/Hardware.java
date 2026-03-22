c04ea2acc701039e75dcdef13323ccbd4688cf47
markdaveoci <markdaveoci@gmail.com>
2026-03-22 09:39:55 +0800

    Insert source code to Hardware class

--- Task4/src/Hardware.java
+++ Task4/src/Hardware.java
@@ -1,3 +1,24 @@
 public class Hardware {
 
+    protected int id;
+    protected String brand;
+    protected int spec;
+
+    public Hardware(int id, String brand, int spec) {
+        this.id = id;
+        this.brand = brand;
+        this.spec = spec;
+    }
+
+    public int getSpec() {
+        return spec;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String interpretSpec() {
+        return spec + " units";
+    }
 }
