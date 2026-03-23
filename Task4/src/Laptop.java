4ccb966c9ac7a1fa5c83848696547efe780b328d
markdaveoci <markdaveoci@gmail.com>
2026-03-23 22:42:56 +0800

    Revert "Laptop Class"
    This reverts commit c45435241f00b5da56689a06f47480d8e3f10e0b.

--- Task4/src/Laptop.java
+++ Task4/src/Laptop.java
@@ -1,37 +1,3 @@
+public class Laptop extends Hardware {
 
-    public class Laptop extends Hardware {
-
-    private Laptop(LaptopBuilder builder) {
-        super(builder.id, builder.brand, builder.spec);
-    }
-
-
-    public String interpretSpec() {
-        return spec + "GB RAM";
-    }
-
-    public static class LaptopBuilder {
-        private int id;
-        private String brand;
-        private int spec;
-
-        public LaptopBuilder setId(int id) {
-            this.id = id;
-            return this;
-        }
-
-        public LaptopBuilder setBrand(String brand) {
-            this.brand = brand;
-            return this;
-        }
-
-        public LaptopBuilder setSpec(int spec) {
-            this.spec = spec;
-            return this;
-        }
-
-        public Laptop build() {
-            return new Laptop(this);
-        }
-    }
 }
